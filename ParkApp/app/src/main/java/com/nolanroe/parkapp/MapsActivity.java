320b3e1d76e880f7370be2ca9f8e142124a746f2
danolanater <danolanater@gmail.com>
2017-09-22 19:27:18 -0700

    changed LatLng on Marker Pin

--- ParkApp/app/src/main/java/com/nolanroe/parkapp/MapsActivity.java
+++ ParkApp/app/src/main/java/com/nolanroe/parkapp/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng sacStateLot7 = new LatLng(38.555790, -121.421106);
+        mMap.addMarker(new MarkerOptions().position(sacStateLot7).title("Marker in Lot 7"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sacStateLot7));
     }
 }
