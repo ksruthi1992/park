a509a77bb44b2132f385ce078c0453535ed70433
danolanater <danolanater@gmail.com>
2017-10-25 23:00:51 -0700

    implemented ValetActivity class

--- ParkApp/app/src/main/java/com/nolanroe/parkapp/ValetActivity.java
+++ ParkApp/app/src/main/java/com/nolanroe/parkapp/ValetActivity.java
@@ -1,10 +1,40 @@
 package com.nolanroe.parkapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 /**
  * Created by Danolanater on 10/23/2017.
  */
 
-public class ValetActivity extends AppCompatActivity{
+public class ValetActivity extends FragmentActivity implements OnMapReadyCallback{
+
+    String dummyLat = "38.5564516";
+    String dummyLon = "-121.421872";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Uri gmmIntentUri = Uri.parse("google.navigation:q=" + dummyLat + "," + dummyLon);
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
+    }
 }
