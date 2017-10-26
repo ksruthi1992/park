a387efcea8929f5f7b0845bbdc1417206388df46
danolanater <danolanater@gmail.com>
2017-10-25 23:58:45 -0700

    created arraylist for buildings at the school

--- ParkApp/app/src/main/java/com/nolanroe/parkapp/NearActivity.java
+++ ParkApp/app/src/main/java/com/nolanroe/parkapp/NearActivity.java
@@ -1,6 +1,13 @@
 package com.nolanroe.parkapp;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 /**
  * Created by Danolanater on 10/23/2017.
@@ -8,6 +15,16 @@ import android.support.v4.app.Fragment;
 
 public class NearActivity extends Fragment{
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.building_list, container, false);
 
+        final ArrayList<String> buildings = new ArrayList<>();
+        buildings.add(new String("Alpine Hall"));
+        buildings.add(new String("Amador Hall"));
+        buildings.add(new String("Your Mom's House"));
 
+        return rootView;
+    }
 }
