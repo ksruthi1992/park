705d7f3ab27e8c5e84fbdd2bfe49309e605217c6
danolanater <danolanater@gmail.com>
2017-10-25 22:09:22 -0700

    added shit to AndroidManifest.xml

--- ParkApp/app/src/main/java/com/nolanroe/parkapp/NearActivity.java
+++ ParkApp/app/src/main/java/com/nolanroe/parkapp/NearActivity.java
@@ -1,8 +1,10 @@
 package com.nolanroe.parkapp;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Danolanater on 10/23/2017.
  */
 
-public class NearActivity {
+public class NearActivity extends AppCompatActivity{
 }
--- ParkApp/app/src/main/java/com/nolanroe/parkapp/ValetActivity.java
+++ ParkApp/app/src/main/java/com/nolanroe/parkapp/ValetActivity.java
@@ -1,8 +1,10 @@
 package com.nolanroe.parkapp;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Danolanater on 10/23/2017.
  */
 
-public class ValetActivity {
+public class ValetActivity extends AppCompatActivity{
 }
