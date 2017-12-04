46c709b101d311c12133aa8a00a9c63e60cdd4f6
Hugo T <hugotavares225@gmail.com>
2017-12-03 19:35:35 -0800

    ?

--- park/ParkDbWalkingSkeleton/src/server/env/environment.js
+++
@@ -1,9 +0,0 @@
-const cosmosPort = 10255; // replace with your port
-const dbName = 'parkdbcsus';
-const key = 'wPkxYoX2a6H4ryBXVHlzVUn5cy6h36SVEXfHc1imVUrPitIsEV7YPyUz1AshghrYPuR8drOsb3L915ApJOV9sg==';
-
-module.exports = {
-  dbName,
-  key,
-  cosmosPort
-};
