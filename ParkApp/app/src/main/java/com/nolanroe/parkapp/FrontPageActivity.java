67085fc45c781e9e435231c0c481adc8096e1af9
Hugo T <hugotavares225@gmail.com>
2017-12-04 22:19:19 -0800

    added working voice functionality to the near activity, need to make it work from front page when parking logo is tapped...

--- ParkApp/app/src/main/java/com/nolanroe/parkapp/FrontPageActivity.java
+++ ParkApp/app/src/main/java/com/nolanroe/parkapp/FrontPageActivity.java
@@ -4,9 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
+import android.widget.ImageButton;
+import android.view.View.OnClickListener;
 
 /**
  * Created by Danolanater on 10/23/2017.
@@ -19,6 +20,16 @@ public class FrontPageActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.front_page_layout);
+        ImageButton theMicButton = (ImageButton) findViewById(R.id.micButton);
+        //final NearActivity start = new NearActivity();
+        theMicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), NearActivity.class);
+                startActivity(i);
+                //start.micButton.performClick();
+            }
+        });
     }
 
     public void buttonPressed(View view)
--- ParkApp/app/src/main/java/com/nolanroe/parkapp/NearActivity.java
+++ ParkApp/app/src/main/java/com/nolanroe/parkapp/NearActivity.java
@@ -1,13 +1,16 @@
 package com.nolanroe.parkapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.annotation.Nullable;
 
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
@@ -21,10 +24,13 @@ import java.util.List;
  * Created by Danolanater on 10/23/2017.
  */
 
-public class NearActivity extends AppCompatActivity {
+public class NearActivity extends Activity implements View.OnClickListener {
 
 
     private List<String> dummyGeoCoords = new ArrayList<>();
+    public ListView mList;
+    public Button micButton;
+    public static final int VOICE_RECOGNITION_REQUEST_CODE = 1234;
 
 
     @Nullable
@@ -32,6 +38,9 @@ public class NearActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.building_list);
+        micButton = (Button) findViewById(R.id.speakButton);
+        micButton.setOnClickListener(this);
+        voiceinputbuttons();
 
 
 
@@ -156,4 +165,139 @@ public class NearActivity extends AppCompatActivity {
         });
     }
 
+    public void voiceinputbuttons() {
+        micButton = (Button) findViewById(R.id.speakButton);
+        mList = (ListView) findViewById(R.id.buildingsList);
+    }
+
+    public void startVoiceRecognitionActivity() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Say The Name of The Building You Wish to Park Near");
+        startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
+    }
+
+    //Voice feature... will add more word variations later...
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK) {
+            ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            mList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, text));
+
+            if (text.contains("Draper Hall") || text.contains("Draper haul") || text.contains("Draper halt") || text.contains("Draper") ||text.contains("traper") || text.contains("trapper")
+                    || text.contains("Sierra") || text.contains("Ciara") || text.contains("Ciera") || text.contains("Cierra") || text.contains("ceara") || text.contains("Sierra Hall") || text.contains("Sierra halt")
+                    || text.contains("Jenkins") || text.contains("Jenkin") || text.contains("Jenkins Hall") || text.contains("Jenkin Hall") || text.contains("Jankins") || text.contains("Jankins Hall")
+                    || text.contains("Desmond Hall") || text.contains("Desmund Hall") || text.contains("Desmond Hall") || text.contains("Desmund") || text.contains("Dezmond Hall") || text.contains("Dezmond")
+                    || text.contains("Dining") || text.contains("dining") || text.contains("dine in") || text.contains("Tainan")) {
+                Toast.makeText(NearActivity.this, "Best Lot Found: LOT 2\nSearching for best parking space...", Toast.LENGTH_SHORT).show(); //
+                dummyGeoCoords.add("38.565985");
+                dummyGeoCoords.add("-121.424618");
+                Intent myIntent = new Intent(this, NavigatorActivity.class);
+                myIntent.putStringArrayListExtra("geoCoords", (ArrayList<String>) dummyGeoCoords);
+                startActivity(myIntent);
+            }
+
+            //Student Lot 2
+            if (text.contains("Shasta Hall")  || text.contains("Shasta") || text.contains("Shazta") || text.contains("Shazta Hall")
+                    || text.contains("River Front Center") || text.contains("River") || text.contains("Front") || text.contains("Center") || text.contains("Riverfront Center") || text.contains("Riverfront Centre") ){
+                Toast.makeText(NearActivity.this, "Best Lot Found: STUDENT LOT 2\nSearching for best parking space...", Toast.LENGTH_SHORT).show(); //
+                dummyGeoCoords.add("38.564094");
+                dummyGeoCoords.add("-121.253284");
+                Intent myIntent = new Intent(this, NavigatorActivity.class);
+                myIntent.putStringArrayListExtra("geoCoords", (ArrayList<String>) dummyGeoCoords);
+                startActivity(myIntent);
+            }
+
+            //Lot 1
+            if (text.contains("Sacramento Hall") || text.contains("Sacramento")
+                    || text.contains("Lassen Hall") || text.contains("Lassen")
+                    || text.contains("Yosemite Hall") || text.contains("Yosemite")
+                    || text.contains("Solano Hall") || text.contains("Solano")
+                    || text.contains("Douglass Hall") || text.contains("Douglass")
+                    || text.contains("Kadema Hall")  || text.contains("Kadema")
+                    || text.contains("Alpine Hall") || text.contains("Alpine")){
+                Toast.makeText(NearActivity.this, "Best Lot Found: LOT 1\nSearching for best parking space...", Toast.LENGTH_SHORT).show(); //
+                dummyGeoCoords.add("38.563318");
+                dummyGeoCoords.add("-121.428851");
+                Intent myIntent = new Intent(this, NavigatorActivity.class);
+                myIntent.putStringArrayListExtra("geoCoords", (ArrayList<String>) dummyGeoCoords);
+                startActivity(myIntent);
+            }
+
+            //Lot 7
+            if (text.contains("El Dorado Hall") || text.contains("Dorado") || text.contains("Dorado Hall")
+                    || text.contains("Art Sculpture Lab") || text.contains("Artsculpture Lab") || text.contains("Artsculpturelab") || text.contains("Art Lab") || text.contains("Artlab") || text.contains("Sculpture lab")
+                    || text.contains("Child Development Center") || text.contains("Child Development") || text.contains("Development Center") || text.contains("Development")){
+                Toast.makeText(NearActivity.this, "Best Lot Found: LOT 7\nSearching for best parking space...", Toast.LENGTH_SHORT).show(); //
+                dummyGeoCoords.add("38.556179");
+                dummyGeoCoords.add("-121.419705");
+                Intent myIntent = new Intent(this, NavigatorActivity.class);
+                myIntent.putStringArrayListExtra("geoCoords", (ArrayList<String>) dummyGeoCoords);
+                startActivity(myIntent);
+            }
+
+            //Lot 8
+            if (text.contains("Union")
+                    || text.contains("Hornet Stadium")  || text.contains("Stadium") || text.contains("Hornetstadium")
+                    || text.contains("Tahoe Hall") || text.contains("Tahoe")
+                    || text.contains("Benecia Hall") || text.contains("Benecia Hall") || text.contains("Bencia Hall") || text.contains("Bencia")) {
+                Toast.makeText(NearActivity.this, "Best Lot Found: LOT 8\nSearching for best parking space...", Toast.LENGTH_SHORT).show(); //
+                dummyGeoCoords.add("38.556152");
+                dummyGeoCoords.add("-121.420716");
+                Intent myIntent = new Intent(this, NavigatorActivity.class);
+                myIntent.putStringArrayListExtra("geoCoords", (ArrayList<String>) dummyGeoCoords);
+                startActivity(myIntent);
+            }
+
+            //Parking Struct 1
+            if (text.contains("Capistrano Hall") || text.contains("Capistrano")
+                    || text.contains("Brighton Hall") || text.contains("Brighton")
+                    || text.contains("Eureka Hall")  || text.contains("Eureka") || text.contains("Yoreka") || text.contains("Yoreka Hall")
+                    || text.contains("Mariposa Hall") || text.contains("Mariposa")
+                    || text.contains("Amador Hall") || text.contains("Amador")) {
+                Toast.makeText(NearActivity.this, "Found: PARKING STRUCTURE 1\nSearching for best parking space...", Toast.LENGTH_SHORT).show(); //
+                dummyGeoCoords.add("38.559441");
+                dummyGeoCoords.add("-121.426895");
+                Intent myIntent = new Intent(this, NavigatorActivity.class);
+                myIntent.putStringArrayListExtra("geoCoords", (ArrayList<String>) dummyGeoCoords);
+                startActivity(myIntent);
+            }
+
+            //Parking Struct 3
+            if (text.contains("The Well")
+                    || text.contains("AIRC") || text.contains("Ark")
+                    || text.contains("Library") || text.contains("Libry")
+                    || text.contains("Placer") || text.contains("Place") || text.contains("Place Hall") || text.contains("Placer Hall")
+                    || text.contains("Homboldt") || text.contains("Hombolt") || text.contains("Homboldt Hall") || text.contains("Hombolt Hall")
+                    || text.contains("Santa Clara Hall") || text.contains("Santaclara Hall") || text.contains("Santa Clara") || text.contains("Santaclara")) {
+                Toast.makeText(NearActivity.this, "Found: PARKING STRUCTURE 3\nSearching for best parking space...", Toast.LENGTH_SHORT).show(); //
+                dummyGeoCoords.add("38.556882");
+                dummyGeoCoords.add("-121.420941");
+                Intent myIntent = new Intent(this, NavigatorActivity.class);
+                myIntent.putStringArrayListExtra("geoCoords", (ArrayList<String>) dummyGeoCoords);
+                startActivity(myIntent);
+            }
+
+            //Parking Struct 2
+            if (text.contains("Bookstore")
+                    || text.contains("Mendocino") || text.contains("Mendcino") || text.contains("Mendocino Hall") || text.contains("Mendcino Hall")
+                    || text.contains("Sequioa") || text.contains("Zequioa") || text.contains("Seqoya") || text.contains("Sequioa Hall") || text.contains("Zequioa Hall") || text.contains("Seqoya Hall")
+                    || text.contains("Riverside Hall") || text.contains("Riverside") || text.contains("River Side") || text.contains("River Side Hall")) {
+                Toast.makeText(NearActivity.this, "Found: PARKING STRUCTURE 2\nSearching for best parking space...", Toast.LENGTH_SHORT).show(); //
+                dummyGeoCoords.add("38.559037");
+                dummyGeoCoords.add("-121.420265");
+                Intent myIntent = new Intent(this, NavigatorActivity.class);
+                myIntent.putStringArrayListExtra("geoCoords", (ArrayList<String>) dummyGeoCoords);
+                startActivity(myIntent);
+            }
+        }
+    }
+
+    public void onClick(View v) {
+        startVoiceRecognitionActivity();
+    }
+
 }
+
